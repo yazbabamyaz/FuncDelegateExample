b59efaed7ca15679a02ec68fb0c2d164203bcfb7
yazbabamyaz <yazilimagirisim@gmail.com>
2023-07-04 17:10:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Bir sayı gir:");
+        int number = Convert.ToInt32(Console.ReadLine());
+        //delegemiz parametre almamaktadır.Geriye de bool döndürür.Son parametre.
+        //=> ifadesinden önce parametre yazılır.PARAMETRE yoksa() yazılır.
+        //scope lar içine geriye bool döndürecek kodlarımızı yazıyoruz.
+        Func<bool> result = () =>
+        {
+            if (number <5)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        };
+        Console.WriteLine(result());
+        Console.ReadLine();
+    }
+}
